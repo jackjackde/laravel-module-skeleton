4939530aff5e98c12ae82d385c41d7fb55f9845c
Stefan <2054008+jackjackde@users.noreply.github.com>
2025-01-27 16:18:47 +0100

    Remove unused namespaces

--- src/Http/Controllers/Index/Index.php
+++ src/Http/Controllers/Index/Index.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace :uc:vendor\:uc:package\Http\Controllers\Index;
 
-use Illuminate\Http\Request;
 use JackJack\Framework\Attributes\RouteAttributes\Get;
 use JackJack\Framework\Attributes\RouteAttributes\Prefix;
 use JackJack\Framework\Http\Controllers\Controller;
-use JackJack\Modules\Modules;
-use JackJackVendor\AuthPackage\AuthPackage;
 
 #[Prefix(':lc:package')]
 class Index extends Controller
